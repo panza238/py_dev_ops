984b31b29ffe75465b5e3b195b0359af4299abce
panza238 <ezequiel.panzarasa@gmail.com>
2023-07-03 23:09:59 -0300

    encapsulated wt_time_benchmarking.py. Added verbosity option to world timer CLI tool

--- utils/wt_time_benchmarking.py
+++ utils/wt_time_benchmarking.py
@@ -9,13 +9,24 @@ import sys
 sys.path.append("/Users/ezequiel.panzarasa/Desktop/Panza/Repos/py_dev_ops")  # PATCH. Just for now
 from world_timer.world_timer import world_timer
 
+# Encapsulate all of this in a function to run from main
 
-# Measure the execution time of the world_timer function
-# 1 - SETUP
-cities = ['America/Buenos_Aires', 'Europe/London', 'America/Lima', 'America/Los_Angeles', 'Europe/Paris']
+def wt_timing():
+    """
+    Measure the execution time of the world_timer function.
+    This function executes the world_timer function multiple times and measures the time it takes to execute.
+    It performs the following steps:
+    1 - SETUP: Initializes a list of cities.
+    2 - EXECUTE: Calls the world_timer function multiple times and measures the execution time using the timeit module.
+    3 - PRINT: Prints the mean and standard deviation of the execution times.
+    """
+    # 1 - SETUP
+    cities = ['America/Buenos_Aires', 'Europe/London', 'America/Lima', 'America/Los_Angeles', 'Europe/Paris']
+    # 2 - EXECUTE
+    results = timeit.repeat(lambda: world_timer(cities), repeat=5, number=10)
+    # 3 - PRINT
+    print(f"Mean: {round(statistics.mean(results), 3)} seconds, Standard Deviation: {round(statistics.stdev(results), 3)} seconds\n")
 
-# 2 - EXECUTE
-results = timeit.repeat(lambda: world_timer(cities), repeat=5, number=10)
 
-# 3 - PRINT
-print(f"Mean: {statistics.mean(results)}, Standard Deviation: {statistics.stdev(results)}\n")
+if __name__ == "__main__":
+    wt_timing()
--- world_timer/world_timer.py
+++ world_timer/world_timer.py
@@ -15,9 +15,8 @@ def list_cities_time(cities_list):
     """list provided cities"""
     base_url = BASE_API_URL + "timezone/"
 
-    for city in cities_list:
-        response = requests.get(base_url + city)
-        click.echo(click.style(f"{city.split('/')[-1]}: {response.json()['datetime']}", fg='green'))
+    response_list = [requests.get(base_url + city).json() for city in cities_list]
+    return response_list
 
 
 def list_current_location_time():
@@ -25,16 +24,22 @@ def list_current_location_time():
     base_url = BASE_API_URL + "/ip"
 
     response = requests.get(base_url).json()
-    click.echo("No cities provided. Using current location")
-    click.echo(click.style(f"{response['timezone'].split('/')[-1]}: {response['datetime']}", fg='green'))
+    return response
 
 
-def world_timer(cities):
+def world_timer(cities, verbose=False):
     """CLI tool to list the current time in different cities"""
     if not cities:
-        list_current_location_time()
+        response = list_current_location_time()
+        if verbose:
+            click.echo("No cities provided. Using current location")
+            click.echo(click.style(f"{response['timezone'].split('/')[-1]}: {response['datetime']}", fg='green'))
     else:
-        list_cities_time(cities_list=cities)
+        responses = list_cities_time(cities_list=cities)
+        if verbose:
+            for response in responses:
+                click.echo(click.style(f"{response['timezone'].split('/')[-1]}: {response['datetime']}", fg='green'))
+            
 
     today = datetime.datetime.today().date().isoformat()
     with open(f"{BASE_WT_LOGS_PATH}/log_file_{today}.log", "a") as log_file:
@@ -48,9 +53,10 @@ def world_timer(cities):
               if more than one city is provided, each city must be preceded by the --cities or -c flag\n
               example: -c America/Buenos_Aires -c Europe/London\n
               If no cities are provided, the current location's time will be displayed""")
-def world_timer_command(cities):
+@click.option("--verbose", "-v", is_flag=True, default=False, help="Enable verbose output")
+def world_timer_command(cities, verbose):
     """CLI tool to list the current time in different cities"""
-    world_timer(cities)
+    world_timer(cities, verbose)
 
 
 if __name__ == "__main__":
