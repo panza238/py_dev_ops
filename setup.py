fa3ccb84be4dfbcc0e1424a38a5b955d5e798a40
panza238 <ezequiel.panzarasa@gmail.com>
2023-07-16 19:06:50 -0300

    added a setup.py file to create a package

---
+++ setup.py
@@ -0,0 +1,19 @@
+"""
+This file holds all the information that setuptools need mae a package out of this module
+"""
+
+from setuptools import setup, find_packages
+setup(
+name="world-timer",
+version="0.1.0",
+author="Ezequiel Panzarasa",
+author_email="ezequiel.panzarasa@gmail.com",
+url="https://github.com/panza238/py_dev_ops",
+description="A world-timer example package",
+packages=find_packages(),
+classifiers=[
+"Programming Language :: Python :: 3",
+"License :: OSI Approved :: MIT License",
+"Operating System :: OS Independent",
+],
+)
